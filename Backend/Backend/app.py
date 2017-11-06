da656fa81905c531e95215f897f961d5c78c06ab
Joel Evans <joelevans@Joels-MBP.wssu.edu>
2017-11-06 11:06:54 -0500

    Add some login stuff and some api stuff to the program

--- Backend/Backend/app.py
+++ Backend/Backend/app.py
@@ -1,27 +1,170 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_httpauth import HTTPBasicAuth
 from flask import request, redirect
-from models import *
+from flask_restplus import Api, Resource, Namespace
+import os
 
-app = Flask(__name__)  # The app itself
+#==============================================================
+# Config of the app
+#==============================================================
+app = Flask(__name__)  # Makes the flask app
+db = SQLAlchemy(app)  # Makes our postgresql database
+api = Api(app)  # Makes a flask restplus api
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://joelevans:@localhost/rameatz'
-db = SQLAlchemy(app)  # The database instance
-app.debug = True
-# db.create_all()
+SECRET_KEY = os.urandom(24)
+app.config['SECRET_KEY'] = SECRET_KEY
+auth = HTTPBasicAuth()
+from models import *  # Get all the parts of the database we need
+
+#==============================================================
+# Auth
+#==============================================================
 
 
-@app.route('/')
-def index():
-    return "Hello World"
+@auth.verify_password
+def verify(username, password):
+    if not (username, password):
+        return False
+    else:
+        user = Students.query.filter_by(username=username)
+        if(user.password == password):
+            return True
+        else:
+            return False
 
 
-@app.route('/post_user', methods=['GET', 'POST'])
-def post_user():
-    user = Users('test', 'test@mail')  # make object
-    db.session.add(user)  # save to database
+#==============================================================
+# Making Name spaces for the APIS
+#==============================================================
+orders_api = Namespace('orders')
+api.add_namespace(orders_api)
+
+
+@orders_api.route('/')
+class OrdersList(Resource):
+    # @auth.login_required
+    # @orders_api.response(UserSchema(many=True))
+    def get(self):
+        student = Students.query.first()
+        return student.as_dict()
+
+
+@orders_api.route('/<int:user_id>')
+# @orders_api.resolve_object('user', lambda kwargs: User.query.get_or_404(kwargs.pop('user_id')))
+class ordersByID(Resource):
+
+    # @users_api.response(UserSchema())
+    def get(self, user):
+        return user
+
+
+@app.route('/burgerStudio')
+def makeVendors():
+    burgerStudio = Vendors('BurgerStudio', 'BurgerStudio@mail.com',
+                           'burgerPassword', 'Burger Studio')
+    db.session.add(burgerStudio)
     db.session.commit()
-    return "Hi"
+    return 'It worked'
+
+    # @app.route('/post_user', methods=['GET', 'POST'])
+    # def post_user():
+    #     user = Users('test', 'test@mail')  # make object
+    #     db.session.add(user)  # save to database
+    #     db.session.commit()
+    #     return "Hi"
+
+    # @app.route('/post_user', methods=['GET', 'POST'])
+    # def post_user():
+    #     user = Users('test', 'test@mail')  # make object
+    #     db.session.add(user)  # save to database
+    #     db.session.commit()
+    #     return "Hi"
+
+
+@api.route('/hello')
+class HelloWorld(Resource):
+    def get(self):
+        return {'hello': 'world'}
+
+
+@app.route('/burgerStudio/comp')
+def makeComponents():
+    vendor = Vendors.query.filter_by(s='Burger Studio').first()
+
+    # Buns
+    whiteBun = Components('White Bun', vendor.id, 100, 0)
+    db.session.add(whiteBun)
+
+    wheatBun = Components('Wheat Bun', vendor.id, 100, 0)
+    db.session.add(wheatBun)
+
+    noBun = Components('No Bun', vendor.id, 100, 0)
+    db.session.add(noBun)
+
+    # Cheeses
+    american = Components('American Cheese', vendor.id, 100, 0)
+    db.session.add(american)
+
+    cheddar = Components('Cheddar Cheese', vendor.id, 100, 0)
+    db.session.add(cheddar)
+
+    swiss = Components('Swiss Cheese', vendor.id, 100, 0)
+    db.session.add(swiss)
+
+    pepperJack = Components('Pepper Jack Cheese', vendor.id, 100, 0)
+    db.session.add(pepperJack)
+
+    provolone = Components('Provolone Cheese', vendor.id, 100, 0)
+    db.session.add(provolone)
+
+    # Toppings
+    bacon = Components('Bacon', vendor.id, 100, 1)
+    db.session.add(bacon)
+
+    doubleBacon = Components('Double Bacon', vendor.id, 100, 2)
+    db.session.add(doubleBacon)
+
+    firedEgg = Components('Fried Egg', vendor.id, 100, 1)
+    db.session.add(firedEgg)
+
+    guacamole = Components('Guacamole', vendor.id, 100, 1)
+    db.session.add(guacamole)
+
+    icebertgLettuce = Components('Icebertg Lettuce', vendor.id, 100, 0)
+    db.session.add(icebertgLettuce)
+
+    tomato = Components('Tomato', vendor.id, 100, 0)
+    db.session.add(tomato)
+
+    pickles = Components('Pickles', vendor.id, 100, 0)
+    db.session.add(pickles)
+
+    redOnions = Components('Red Onions', vendor.id, 100, 0)
+    db.session.add(redOnions)
+
+    carmOnions = Components('Carmelized Onions', vendor.id, 100, 0)
+    db.session.add(carmOnions)
+
+    sauteedMush = Components('Sauteed Muchrooms', vendor.id, 100, 0)
+    db.session.add(sauteedMush)
+
+    sauttedBellPeppers = Components('Sauteed Bell Peppers', vendor.id, 100, 0)
+    db.session.add(sauttedBellPeppers)
+
+    jalapenos = Components('Jalapenos', vendor.id, 100, 0)
+    db.session.add(jalapenos)
+
+    salsa = Components('Salsa', vendor.id, 100, 0)
+    db.session.add(salsa)
+
+    picoDeGallo = Components('Pico De Gallo', vendor.id, 100, 1)
+    db.session.add(salsa)
+
+    db.session.commit()
+
+    return vendor.username
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
--- Backend/Backend/models.py
+++ Backend/Backend/models.py
@@ -1,24 +1,23 @@
-from flask_sqlalchemy import SQLAlchemy
-from app import app
-
-db = SQLAlchemy(app)
-
+from app import db
 
 '''
     Objects that needed to made:
+        Note to
         Orders(need to have filds to check what queue it is in)
         Items for inventory
-        popular meals for socail
         questions
         promos
 '''
+
+# Note Might need to refacotr the users so they inheirht form the same parent class
 
 
 class Students(db.Model):
     # neeed to chagne this os it is a UUID
     id = db.Column(db.Integer, primary_key=True)
-
     username = db.Column(db.String(80), unique=True)
+    firstName = db.Column(db.String(80))
+    lastName = db.Column(db.String(80))
     email = db.Column(db.String(120), unique=True)
     password = db.Column(db.String(120))  # needs to be hashed
     decliningBal = db.Column(db.Float)
@@ -40,75 +39,202 @@ class Students(db.Model):
     def __reper__(self):
         return '<User %r>' % self.username
 
+    def checkMealSwipes(self):
+        return self.mealSwipes
 
-class Cooks(db.Model):
+    def checkDC(self):
+        return self.decliningBal
+
+    def as_dict(self):
+        return {c.name: getattr(self, c.name) for c in self.__table__.columns}
+
+
+class Vendors(db.Model):
     # neeed to chagne this os it is a UUID
     id = db.Column(db.Integer, primary_key=True)
-
     username = db.Column(db.String(80), unique=True)
     email = db.Column(db.String(120), unique=True)
     password = db.Column(db.String(120))  # needs to be hashed
+    storeName = db.Column(db.String(120), unique=True)
+    items = db.relationship("Items", backref='vendorItems', lazy=True)
+    components = db.relationship("Components", backref='vendorComponents', lazy=True)
+    meals = db.relationship("Meal", backref='vendorMeals', lazy=True)
 
-    '''
-        *Needs a forigetn key to the meals that they choose to cook and not
-        what other cooks are making
-        *Needs a forigen key to the vendor they work for so can only see meals
-        from that vendor
-    '''
-
-    def __init__(self, username, email, password):
+    def __init__(self, username, email, password, storeName):
         self.username = username
         self.email = email
         self.password = password
+        self.storeName = storeName
 
-    def __reper__(self):
-        return '<User %r>' % self.username
-
-
-class Devs(db.Model):
-    # neeed to chagne this os it is a UUID
-    id = db.Column(db.Integer, primary_key=True)
-
-    username = db.Column(db.String(80), unique=True)
-    email = db.Column(db.String(120), unique=True)
-    password = db.Column(db.String(120))  # needs to be hashed
-
-    '''
-        *They are the super suers of the system and can add and remvoe users
-        *Needs a key to the queston that they have answered
-            Note they still can see all the questions however
-
-    '''
-
-    def __init__(self, username, email, password):
-        self.username = username
-        self.email = email
-        self.password = password
-
-    def __reper__(self):
-        return '<User %r>' % self.username
-
-
-class vendors(db.Model):
-    # neeed to chagne this os it is a UUID
-    id = db.Column(db.Integer, primary_key=True)
-
-    username = db.Column(db.String(80), unique=True)
-    email = db.Column(db.String(120), unique=True)
-    password = db.Column(db.String(120))  # needs to be hashed
-    storeName = db.Column(db.String(120))
+    def as_dict(self):
+        return {c.name: getattr(self, c.name) for c in self.__table__.columns}
 
     '''
         *Needs to have a key to its meneu 1 to 1
         *Need to be linked to their promotions
         *Need to be conntected with all the items in their inventory Many to Many
-        
+
     '''
 
-    def __init__(self, username, email, password):
-        self.username = username
-        self.email = email
-        self.password = password
 
-    def __reper__(self):
-        return '<User %r>' % self.username
+# Many to Many realtionship between Meals and Items
+orderMeals = db.Table('orderMeals',
+                      db.Column('orderID', db.Integer, db.ForeignKey(
+                          'orders.id'), primary_key=True),
+                      db.Column('mealID', db.Integer, db.ForeignKey(
+                          'meal.id'), primary_key=True),
+                      )
+
+orderItems = db.Table('orderItems',
+                      db.Column('orderID', db.Integer, db.ForeignKey(
+                          'orders.id'), primary_key=True),
+                      db.Column('itemID', db.Integer, db.ForeignKey(
+                          'items.id'), primary_key=True)
+                      )
+
+
+class Orders(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    student = db.Column(db.Integer, db.ForeignKey(
+        'students.id'), nullable=False)
+    price = db.Column(db.Float)
+    meals = db.relationship('Meal', secondary=orderMeals,
+                            lazy='subquery', backref=db.backref('orders', lazy=True))
+    items = db.relationship('Items', secondary=orderItems,
+                            lazy='subquery', backref=db.backref('orders', lazy=True))
+
+    def as_dict(self):
+        return {c.name: getattr(self, c.name) for c in self.__table__.columns}
+
+
+# Many to Many realtionship between Meals and Items
+mealItems = db.Table('mealItems',
+                     db.Column('mealID', db.Integer, db.ForeignKey(
+                         'meal.id'), primary_key=True),
+                     db.Column('itemID', db.Integer, db.ForeignKey(
+                         'items.id'), primary_key=True)
+
+                     )
+
+mealComponents = db.Table('mealComponents',
+                          db.Column('componentsID', db.Integer, db.ForeignKey(
+                              'components.id'), primary_key=True),
+                          db.Column('mealID', db.Integer, db.ForeignKey(
+                              'meal.id'), primary_key=True),
+
+                          )
+
+
+class Meal(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    vendor = db.Column(db.Integer, db.ForeignKey('vendors.id'), nullable=False)
+    price = db.Column(db.Float)
+    items = db.relationship('Items', secondary=mealItems,
+                            lazy='subquery', backref=db.backref('meal', lazy=True))
+    components = db.relationship('Components', secondary=mealComponents,
+                                 lazy='subquery', backref=db.backref('items', lazy=True))
+
+    def __init__(self, vendor, price):
+        self.vendor = vendor
+        self.price = price
+
+    def as_dict(self):
+        return {c.name: getattr(self, c.name) for c in self.__table__.columns}
+
+
+
+# Many to Many realtionship between Meals and Items
+itemComponents = db.Table('itemComponents',
+                          db.Column('componentsID', db.Integer, db.ForeignKey(
+                              'components.id'), primary_key=True),
+                          db.Column('itemID', db.Integer, db.ForeignKey(
+                              'items.id'), primary_key=True)
+
+                          )
+
+
+class Items(db.Model):  # EX buns, paty, etc
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120))
+    vendor = db.Column(db.Integer, db.ForeignKey('vendors.id'), nullable=False)
+    stock = db.Column(db.Integer)
+    description = db.Column(db.String(200))
+    price = db.Column(db.Float)
+    components = db.relationship('Components', secondary=itemComponents, lazy='subquery',
+                                 backref=db.backref('itemsComp', lazy=True))
+
+    def as_dict(self):
+        return {c.name: getattr(self, c.name) for c in self.__table__.columns}
+
+
+class Components(db.Model):  # EX buns, paty, etc
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), unique=True)
+    vendor = db.Column(db.Integer, db.ForeignKey('vendors.id'), nullable=False)
+    stock = db.Column(db.Integer)
+    description = db.Column(db.String(200))
+    price = db.Column(db.Float)
+
+    def __init__(self, name, vendor, stock, price):
+        self.name = name
+        self.vendor = vendor
+        self.stock = stock
+        self.price = price
+
+    def as_dict(self):
+        return {c.name: getattr(self, c.name) for c in self.__table__.columns}
+
+# class Cooks(db.Model):
+#     # neeed to chagne this os it is a UUID
+#     id = db.Column(db.Integer, primary_key=True)
+
+#     username = db.Column(db.String(80), unique=True)
+#     email = db.Column(db.String(120), unique=True)
+#     password = db.Column(db.String(120))  # needs to be hashed
+
+#     '''
+#         *Needs a forigetn key to the meals that they choose to cook and not
+#         what other cooks are making
+#         *Needs a forigen key to the vendor they work for so can only see meals
+#         from that vendor
+#     '''
+
+#     def __init__(self, username, email, password):
+#         self.username = username
+#         self.email = email
+#         self.password = password
+
+#     def __reper__(self):
+#         return '<User %r>' % self.username
+
+
+# class Devs(db.Model):
+#     # neeed to chagne this os it is a UUID
+#     id = db.Column(db.Integer, primary_key=True)
+
+#     username = db.Column(db.String(80), unique=True)
+#     email = db.Column(db.String(120), unique=True)
+#     password = db.Column(db.String(120))  # needs to be hashed
+
+#     '''
+#         *They are the super suers of the system and can add and remvoe users
+#         *Needs a key to the queston that they have answered
+#             Note they still can see all the questions however
+
+#     '''
+
+#     def __init__(self, username, email, password):
+#         self.username = username
+#         self.email = email
+#         self.password = password
+
+#     def __reper__(self):
+#         return '<User %r>' % self.username
+
+#     def __init__(self, username, email, password):
+#         self.username = username
+#         self.email = email
+#         self.password = password
+
+#     def __reper__(self):
+#         return '<User %r>' % self.username
