5aa12b0a8d2e9d37edd7b00235de34b9ab5eadf5
Joel Evans <joelevans@Joels-MacBook-Pro.local>
2017-11-20 21:35:02 -0500

    Major Chagnes

--- FrontEnd/RamEatz/src/app/app.module.ts
+++ FrontEnd/RamEatz/src/app/app.module.ts
@@ -2,45 +2,43 @@ import { NgModule, ErrorHandler } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
 
-import { ItemListService } from "../services/item/item.service";
-import { ToastService } from "../services/toast/toast.service";
-import { loginService } from "../services/login/login.service";
-
+import { AboutPage } from '../pages/about/about';
+import { ContactPage } from '../pages/contact/contact';
+import { HomePage } from '../pages/home/home';
+import { TabsPage } from '../pages/tabs/tabs';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
-import { AngularFireModule } from 'angularfire2'
-import { FIREBASE_CREDENTIALS } from "./firebase.credentials";
-import { AngularFireDatabaseModule } from "angularfire2/database";
-import { AngularFireAuthModule } from "angularfire2/auth";
+import { RestProvider } from '../providers/rest/rest';
 
 @NgModule({
   declarations: [
     MyApp,
+    AboutPage,
+    ContactPage,
+    HomePage,
+    TabsPage
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp),
-    //Int Firebase with credientails 
-    AngularFireModule.initializeApp(FIREBASE_CREDENTIALS),
-    //Import Angular FireDatabase to use database interactions
-    AngularFireDatabaseModule,
-    AngularFireAuthModule,
-
+    HttpClientModule,
+    IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
+    AboutPage,
+    ContactPage,
+    HomePage,
+    TabsPage
   ],
   providers: [
     StatusBar,
     SplashScreen,
     { provide: ErrorHandler, useClass: IonicErrorHandler },
-    ItemListService,
-    ToastService,
-    loginService,
-
+    RestProvider
   ]
 })
 export class AppModule { }
---
+++ Old/FrontEnd/RamEatz/src/pages/home/home.ts
@@ -0,0 +1,114 @@
+import { Component } from '@angular/core';
+import { NavController, ActionSheetController, IonicPage } from 'ionic-angular';
+
+//Import the observable and Database
+import { Observable } from 'rxjs/Observable'
+import { Item } from "../../models/Item/item-interface";
+
+import { ItemListService } from "../../services/item/item.service";
+
+
+
+//Ionic Page dec allows us lazy load the page 
+@IonicPage()
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+
+  //The obseravle that is of type item list
+  itemList: Observable<any[]>
+
+  //Import the database here
+  constructor(public navCtrl: NavController,
+    private actions: ActionSheetController,
+    private List: ItemListService, ) {
+
+    /*We are point the itemlist ref at firebse -> 'item list node'
+      Not only can we push to data base we have accues to everything insdie that node
+    */
+    // this.ItemRef$ = this.database.list('item-list');
+
+    this.itemList = this.List
+      .getItemList() //Get the DataBase list
+      .snapshotChanges(). //Get Key and value Pairs
+      map( //Maps these cahgnes and for each one retrun a new obejct
+      changes => {
+        return changes.map(c => ({
+          key: c.payload.key,
+          ...c.payload.val()
+        }));
+      });
+
+
+    //For loging in
+    if (!this.isLoggedin()) {
+      console.log("You are not logged in");
+      this.navCtrl.push("LogInPage")
+    }
+
+
+
+  }
+
+  selectItem(item: Item) {
+    /* Display action sheet that lets the user
+      1. Edit the Item
+      2.Delete Item
+      3.Canacel Selection
+
+    */
+
+    this.actions.create({
+      //Bck tick lets us use a template string
+      title: `${item.itemName}`,
+      buttons: [
+        {
+          text: 'Edit',
+          //Lets us add a function to the button
+          handler: () => { }
+        },
+        {
+          text: 'Delete',
+          role: 'destructive',
+          handler: () => {
+            //Deltes the item the $key is for the actual firebase key
+            console.log(
+              item.localKey);
+            // this.ItemRef.remove(item.localKey);
+          }
+        },
+        {
+          text: 'Cancel',
+          role: 'Cancel',
+          handler: () => console.log('Cancel has been pressed')
+        }
+      ]
+    }).present();
+  }
+
+  navToAddItemPage() {
+    //Navages the useres to the add Item page
+    //if use set root instead of push there would be no back button
+    console.log(window.localStorage.getItem('currentUser'));
+    this.navCtrl.push("AddItemPage");
+  }
+
+  navToEditItemPage() {
+    this.navCtrl.push("EditItemPage");
+
+  }
+
+  navToLoginItemPage() {
+    this.navCtrl.push("LogInPage");
+  }
+
+  isLoggedin() {
+    if (window.localStorage.getItem('currentUser')) {
+      return true;
+    }
+  }
+
+
+}
